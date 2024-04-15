23823830d82b20d1aa1044072dabec9c23db3d14
sujipark <sujipark@hivelab.co.kr>
2024-04-15 22:24:29 +0900

    add notion api (add notion page)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,44 @@
-import List from '@/components/List';
+// import List from '@/components/List';
+// import { notion } from '@/app/lib/notion.ts';
+// import { NotionPage } from '@/components/notion/renderer.tsx';
 
-export const products = [
-  { name: 'Iphone15Pro' },
-  { name: 'Iphone15' },
-  { name: 'Iphone14' },
-];
+// export const products = [
+//   { name: 'Iphone15Pro' },
+//   { name: 'Iphone15' },
+//   { name: 'Iphone14' },
+// ];
 
-export default function Home() {
+// const pageId = process.env.NOTION_DATABASE_ID!;
+
+// async function getData(rootPageId: string) {
+//   return notion.getPage(rootPageId);
+// }
+
+// const data = getData(pageId);
+
+// export default function Home() {
+//   return (
+//     <div className="flex justify-center p-10">
+//       {/* <List products={products} /> */}
+//       <NotionPage recordMap={data} rootPageId={pageId} />
+//     </div>
+//   );
+// }
+
+
+
+import { notion } from "@/app/lib/notion";
+import { NotionPage } from "@/components/notion";
+
+const rootPageId = process.env.NOTION_DATABASE_ID!;
+
+async function getData(rootPageId:string) {
+  return await notion.getPage(rootPageId);
+}
+export default async function Home() {
+  const data = await getData(rootPageId);
+
   return (
-    <div className="flex justify-center p-10">
-      <List products={products} />
-    </div>
+    <NotionPage recordMap={data} rootPageId={rootPageId} />
   );
 }
