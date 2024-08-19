45931c22892b196568776c2567213bb4526d98ac
suji.park <suji.park@konantech.com>
2024-08-19 13:52:44 +0900

    remove key props of productItem component

--- src/app/_components/ProductItem.tsx
+++ src/app/_components/ProductItem.tsx
@@ -1,33 +1,31 @@
-import {ProductProps} from "@/lib/types/product";
+import { ProductProps } from '@/lib/types/product';
 import Link from 'next/link';
 import Image from 'next/image';
-import formatCurrency from "@/app/_utils/formatCurrency";
+import formatCurrency from '@/app/_utils/formatCurrency';
 
 interface ProductItemProps {
-  ItemData: ProductProps
+  ItemData: ProductProps;
 }
 
-function ProductItem (
-  { ItemData }: ProductItemProps
-) {
+function ProductItem({ ItemData }: ProductItemProps) {
   const { id, name, brand, price, imgSrc, tags, slug } = ItemData;
 
-  const colorVariants:any = {
+  const colorVariants: any = {
     blue: 'bg-blue-200',
     red: 'bg-red-200',
     gray: 'bg-neutral-200',
-  }
+  };
 
   return (
-    <div key={id}>
+    <>
       <Link href={`/product/${slug}`} passHref>
         <div className="text-sm">
-          <div className="relative w-full h-0 pb-[100%]">
+          <div className="relative h-0 w-full pb-[100%]">
             <Image
               height={200}
               width={200}
               src={imgSrc ? imgSrc : ''}
-              className="absolute top-0 left-0 w-full h-full object-cover"
+              className="absolute left-0 top-0 h-full w-full object-cover"
               alt="thumbnail"
             />
           </div>
@@ -35,15 +33,19 @@ function ProductItem (
             <strong className="font-semibold">{brand}</strong>
             <p className="pt-1">{name}</p>
             <p className="pt-1">{formatCurrency(price)}</p>
-            {tags && 
-            <p className="pt-1">
-              <span className={`inline-block px-1 ${colorVariants[tags.color]} text-[10px] leading-[14px]`}>{tags.name}</span>
-            </p>
-            }
+            {tags && (
+              <p className="pt-1">
+                <span
+                  className={`inline-block px-1 ${colorVariants[tags.color]} text-[10px] leading-[14px]`}
+                >
+                  {tags.name}
+                </span>
+              </p>
+            )}
           </div>
         </div>
       </Link>
-    </div>
+    </>
   );
 }
 
