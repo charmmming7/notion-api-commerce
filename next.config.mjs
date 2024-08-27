96fd41644f76800866d7afd1fd4974acbd10f326
suji park <charmmmming7@gmail.com>
2024-08-27 17:54:16 +0900

    next.config.mjs 설정에서 compilerOptions 제거

--- next.config.mjs
+++ next.config.mjs
@@ -15,17 +15,11 @@ const nextConfig = {
     ],
     unoptimized: true, // next 이미지 최적화 옵션 off (정적 파일만을 사용하는 SSG 방식에서는 불가능하므로 off)
   },
-  compilerOptions: {
-    baseUrl: '.',
-    paths: {
-      '@/*': ['src/*'],
-    },
-  },
   reactStrictMode: true,
-  webpack: config => {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
 
     return config;
